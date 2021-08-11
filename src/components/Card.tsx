0cb08f843093db4e9cc1531926779bd8bd628aee
Tal Piven <talpivena9@gmail.com>
2021-08-12 02:20:20 +0300

    styled dashboard + card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,27 +4,30 @@ const useStyles = makeStyles({
   root: {
     backgroundColor: "var(--main-color)",
     width: "95%",
-    color: "white",
     maxWidth: "35rem",
-    margin: "1rem auto",
-    padding: "0.7rem",
+    margin: "1.5rem auto",
+    padding: "1rem 1.3rem 1rem 1.3rem",
     borderRadius: "10px",
+  },
+  header: {
     display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: "1.5rem",
   },
   status: {
-    padding: "0.2rem",
-    width: "100%",
     borderRadius: "10px",
-    border: "1px solid grey",
-    marginBottom: "0.5rem",
+    border: "1px solid black",
+    padding: "0.3rem",
   },
-  label: {
-    borderRadius: "10px",
-    border: "1px solid grey",
-    padding: "0.2rem",
+  description: { textAlign: "left" },
+  line: {
+    border: "1px solid var(--secondary-color)",
   },
+  author: { marginRight: "10px", textAlign: "right" },
+  label: {},
   labels: { marginTop: "0.5rem" },
 });
 const PullReqCard = (props: any) => {
@@ -32,16 +35,23 @@ const PullReqCard = (props: any) => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <h2>{title}</h2>
-      <span className={classes.status}>Status: {status}</span>
-      <p>{description}</p>
-      <span>Author: {author}</span>
+      <header className={classes.header}>
+        <span className={classes.title}>{title}</span>
+        <span>
+          Status: <span className={classes.status}>{status}</span>
+        </span>
+      </header>
+      <hr className={classes.line} />
+      <p className={classes.description}>{description}</p>
+      <span className={classes.author}>Author: {author}</span>
       <span>created at: {createdAt}</span>
-      <div className={classes.labels}>
-        {labels.map((label: string) => (
-          <span className={classes.label}>{label}</span>
-        ))}
-      </div>
+      <footer>
+        <div className={classes.labels}>
+          {labels.map((label: string) => (
+            <span className={classes.label}>{label}</span>
+          ))}
+        </div>
+      </footer>
     </Card>
   );
 };
--- src/components/Dash.tsx
+++ src/components/Dash.tsx
@@ -1,12 +1,27 @@
 import { useSelector } from "react-redux";
 import Card from "./Card";
 import SearchBar from "./SearchBar";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "2.5rem",
+    margin: "0",
+  },
+  logo: {
+    height: 60,
+    width: 60,
+  },
+});
 
 const Dashboard = () => {
+  const classes = useStyles();
   const pullRequests = useSelector((state: any) => state.pullRequests);
   return (
-    <div>
-      dashboard
+    <div className={classes.root}>
+      <h1>Pull Request Dashboard</h1>
       <SearchBar />
       {pullRequests.map((pullRequest: any) => (
         <Card
